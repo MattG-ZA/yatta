8ea0c859375e3f260c33540aea8e3b96b78d9c1f
Matthew Gleed <matthew.gleed@kurtosys.com>
2020-01-13 14:08:56 +0200

    Updates

--- precache-manifest.944798f7a40406237275227e767bb85d.js
+++ precache-manifest.944798f7a40406237275227e767bb85d.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48f21117038eaae09fda2704b4f5dd05",
+    "revision": "e21d8fbdaa2a73b8f8dabb014720d415",
     "url": "/yatta/index.html"
   },
   {
-    "revision": "cfabe51265d36c16d9b2",
-    "url": "/yatta/static/css/main.536128f3.chunk.css"
+    "revision": "421d0863ab38e465546a",
+    "url": "/yatta/static/css/main.f5059200.chunk.css"
   },
   {
     "revision": "1bbefa5ba5e310faa990",
     "url": "/yatta/static/js/2.afc972c8.chunk.js"
   },
   {
-    "revision": "cfabe51265d36c16d9b2",
+    "revision": "421d0863ab38e465546a",
     "url": "/yatta/static/js/main.3de5aa12.chunk.js"
   },
   {
