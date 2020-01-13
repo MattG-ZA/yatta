03388250e5b2123101e2da5b12d0ebad14f9cfe5
Matthew Gleed <matthew.gleed@kurtosys.com>
2020-01-13 14:03:20 +0200

    Updated card styling and added card overlays.

--- src/components/gameCard/GameCard.js
+++ src/components/gameCard/GameCard.js
@@ -6,9 +6,23 @@ import { NumberWithCommas } from '../../util/StringHelpers';
 import { NavLink } from 'react-router-dom';
 
 class GameCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isMouseInside: false };
+    }
+
+    mouseEnter = () => {
+        this.setState({ isMouseInside: true });
+    }
+
+    mouseLeave = () => {
+        this.setState({ isMouseInside: false });
+    }
+
     render() {
         const { game, topGame } = this.props;
- 
+
         if (game) {
             const gameCardImageClass = game.usingTwitchImage ? 'card-image' : 'card-mixer-image';
             const cardHeight = topGame ? '380px' : '250px';
@@ -17,10 +31,23 @@ class GameCard extends React.Component {
             return (
                 <div className='card'>
                     <NavLink to={{ pathname: '/streams', state: { gameprops: game } }}>
-                        <span className='card-image-container'>
+                        <span className='card-image-container' onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
                             <img className={gameCardImageClass} src={game.image} style={{ height: cardHeight, width: cardWidth }} alt='GameImage' />
                             {!game.usingTwitchImage &&
                                 <img className='card-background' src={game.image} style={{ height: cardHeight, width: cardWidth }} alt='ImageBackground' />
+                            }
+                            {!topGame && this.state.isMouseInside &&
+                                <span className='card-overlay'>
+                                    <div className='card-title'>{game.name}</div>
+                                    <div>
+                                        <img className='icon' src={TwitchIcon} alt='TwitchIcon' />
+                                        <span className='viewers'>{NumberWithCommas(game.twitchViewers)} viewers</span>
+                                    </div>
+                                    <div>
+                                        <img className='icon' src={MixerIcon} alt='MixerIcon' />
+                                        <span className='viewers'>{NumberWithCommas(game.mixerViewers)} viewers</span>
+                                    </div>
+                                </span>
                             }
                         </span>
                     </NavLink>
--- src/components/gameContainer/ui/TopGamesContainer.js
+++ src/components/gameContainer/ui/TopGamesContainer.js
@@ -6,12 +6,15 @@ const TopGamesContainer = (props) => {
     const { topGames } = props;
 
     return (
-        <span className='top-games'>
-            {
-                topGames.map((game, index) => {
-                    return <GameCard key={index} game={game} topGame={true} />
-                })
-            }
+        <span>
+            <span className='top-games-header'>Top Games</span>
+            <span className='top-games'>
+                {
+                    topGames.map((game, index) => {
+                        return <GameCard key={index} game={game} topGame={true} />
+                    })
+                }
+            </span>
         </span>
     );
 }
