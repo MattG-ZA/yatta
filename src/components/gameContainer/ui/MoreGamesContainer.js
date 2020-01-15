f8203d10b03713f62427000daf6807f7d42085e8
MattG-ZA <gleedmatt@gmail.com>
2020-01-15 23:40:02 +0200

    Added dummy cards to fix last row spacing issues.

--- src/components/gameContainer/ui/MoreGamesContainer.js
+++ src/components/gameContainer/ui/MoreGamesContainer.js
@@ -4,6 +4,12 @@ import GameCard from '../../gameCard/GameCard';
 
 const MoreGamesContainer = (props) => {
     const { moreGames } = props;
+    const dummyCards = [];
+
+    // Add a row of dummy cards to allow the last row to always spread correctly across the container
+    for (let i = 0; i < 7; i++) {
+        dummyCards.push(<span key={`dummy${i}`} className='dummy-card' />);
+    }
 
     return (
         <span className='more-games'>
@@ -11,6 +17,9 @@ const MoreGamesContainer = (props) => {
                 moreGames.map((game, index) => {
                     return <GameCard key={index} game={game} topGame={false} />
                 })
+            }
+            {
+                dummyCards
             }
         </span>
     );
