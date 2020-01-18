e47beca7e560ff5917b275789feb5eac41144ff9
MattG-ZA <gleedmatt@gmail.com>
2020-01-18 23:33:42 +0200

    Added Side Bar expand and collapse functionality.

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './SideBar.css';
+import LeftChevron from '../../resources/LeftChevron.png';
+import YattaLogo from '../../resources/YattaLogo.png';
 
 class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { expanded: true };
+    }
+
+    ToggleSideBar = () => {
+        this.setState({ expanded: !this.state.expanded });
+    }
+
     render() {
+        const sideBarClass = this.state.expanded ? 'side-bar' : 'side-bar-collapsed';
+        const headerClass = this.state.expanded ? 'side-bar-header' : 'side-bar-header-hidden';
+        const collapseClass = this.state.expanded ? 'side-bar-collapse' : 'side-bar-collapse-hidden';
+
         return (
-            <span className='side-bar'>
-                <span className='side-bar-header'>y a t t a</span>
+            <span className={sideBarClass}>
+                <span className={headerClass}>y a t t a</span>
+                <img className={collapseClass} src={LeftChevron} alt='LeftChevron' onClick={this.ToggleSideBar} />
+                {!this.state.expanded && <img className='side-bar-expand' src={YattaLogo} alt='YattaLogo' onClick={this.ToggleSideBar} />}
             </span>
         );
     }
