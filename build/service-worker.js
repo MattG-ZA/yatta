b52ec6717d8a96fcd87ebea9198b5b437d8c98c4
MattG-ZA <gleedmatt@gmail.com>
2020-01-21 19:40:14 +0200

    Search game functionality WIP.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yatta/precache-manifest.339c9b4c06bee77800a18a4dc7b9a419.js"
+  "/yatta/precache-manifest.3a0bec271bade5d1d775c7ec102b8e2a.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/sideBar/ui/Search.js
+++ src/components/sideBar/ui/Search.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import '../SideBar.css';
+import { GetSingleTwitchGame, GetSingleMixerGame } from '../../../util/Api';
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.SearchGame = this.SearchGame.bind(this);
+    }
+
+    async SearchGame(e) {
+        if (e.key === 'Enter') {
+            const searchTerm = e.target.value;
+            console.log('searchTerm ', searchTerm);
+            const twitchGameData = await GetSingleTwitchGame(searchTerm);
+            const mixerGameData = await GetSingleMixerGame(searchTerm);
+            console.log('twitchGameData ', twitchGameData);
+            console.log('mixerGameData ', mixerGameData);
+        }
+    }
+
     render() {
         const { expanded } = this.props;
 
@@ -9,7 +27,7 @@ class Search extends React.Component {
 
         return (
             <span className={searchClass}>
-                <input className='search' placeholder='Search' />
+                <input className='search' placeholder='Search' onKeyDown={this.SearchGame} />
             </span>
         );
     }
